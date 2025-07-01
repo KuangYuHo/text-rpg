e749ae6d24e4ac7e589f8b8fbac41304fa2400b5
KuangYuHo <kevin06026@gmail.com>
2025-07-01 14:55:09 +0800

    create project

---
+++ src/main/java/com/rpg/textrpg/TextRpgApplication.java
@@ -0,0 +1,13 @@
+package com.rpg.textrpg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextRpgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TextRpgApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rpg/textrpg/TextRpgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rpg.textrpg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextRpgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
